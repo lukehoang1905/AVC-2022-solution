72bb16fb77693f1293f5cc1a4ccb577ac72808da
Luke Hoang <61900428+lukehoang1905@users.noreply.github.com>
2022-12-14 11:07:49 +0700

    part 2

--- Day1.js
+++ Day1.js
@@ -1,10 +1,17 @@
+// Part 1
+
 const inputString=document.getElementsByTagName("pre")[0].innerHTML
 const inputArray=inputString.split("\n\n")
 
-inputArray.map((e) => {
+const totalArray=inputArray.map((e) => {
   const newE = e
     .split("\n")
     .reduce((sub, f) => sub + (parseInt(f) ? parseInt(f) : 0), 0);
   if (newE > max) max = newE;
   return newE;
 });
+
+
+// Part 2
+const sortedArray=totalArray.sort((a, b) => b - a)
+const top3Total=sortedArray[0] + sortedArray[1] + sortedArray[2]
