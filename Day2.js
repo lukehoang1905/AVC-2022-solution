faff57deb9017834200c092dc6a876a4cc77e649
Luke Hoang <61900428+lukehoang1905@users.noreply.github.com>
2022-12-14 13:17:46 +0700

    Refactor with possibility

--- Day2.js
+++ Day2.js
@@ -18,20 +18,41 @@ const resultScore = {
 };
 const results = new Map();
 
-const draws = ["A X", "B Y", "C Z"];
+const choiceScore = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+const resultScore = {
+  win: 6,
+  draw: 3,
+  lose: 0,
+};
+const results = new Map();
 
-draws.forEach((e) =>
-  results.set(e, resultScore.draw + choiceScore[e.charAt(2)])
+const possibilities = [
+  { type: "draw", patterns: ["A X", "B Y", "C Z"] },
+  { type: "win", patterns: ["A Y", "B Z", "C X"] },
+  { type: "lose", patterns: ["A Z", "B X", "C Y"] },
+];
+
+//Time constant 
+
+possibilities.forEach((possibility) =>
+  possibility.patterns.forEach((pattern) =>
+    results.set(
+      pattern,
+      resultScore[possibility.type] + choiceScore[pattern.charAt(2)]
+    )
+  )
 );
-const wins = ["A Y", "B Z", "C X"];
 
-wins.forEach((e) => results.set(e, resultScore.win + choiceScore[e.charAt(2)]));
+// Time O(n) ?
+const final = input.reduce((total, game) => {
+  return total + (game.length < 2 ? 0 : results.get(game));
+}, 0);
 
-const losses = ["A Z", "B X", "C Y"];
 
-losses.forEach((e) =>
-  results.set(e, resultScore.lose + choiceScore[e.charAt(2)])
-);
 
 const final = inputArray.reduce((total, game) => {
   return total + (game.length < 2 ? 0 : results.get(game));
